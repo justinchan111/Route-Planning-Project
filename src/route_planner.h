9b2c4f12a3c6de48dfb44e6e7651f8f3f55997b6
justinchan111 <justinchan111@yahoo.com>
2021-07-01 11:26:17 +0100

    First commit

---
+++ src/route_planner.h
@@ -0,0 +1,39 @@
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "route_model.h"
+
+class RoutePlanner {
+  public:
+    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+    // Add public variables or methods declarations here.
+    float GetDistance() const {return distance;}
+    void AStarSearch();
+
+    // The following methods have been made public so we can test them individually.
+    void AddNeighbors(RouteModel::Node *current_node);
+    float CalculateHValue(RouteModel::Node const *node);
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+    RouteModel::Node *NextNode();
+
+  private:
+    // Add private variables or methods declarations here.
+    std::vector<RouteModel::Node*> open_list;
+    RouteModel::Node *start_node;
+    RouteModel::Node *end_node;
+
+    float distance = 0.0f;
+    RouteModel &m_Model;
+};
+
+// Sort the 1-D vector of nodes in descending order relative to the f-value.
+void NodeSort(std::vector<RouteModel::Node*> *v);
+
+// Compare the f-values of two nodes.
+bool Compare(RouteModel::Node* node1, RouteModel::Node* node2);
+
+#endif
